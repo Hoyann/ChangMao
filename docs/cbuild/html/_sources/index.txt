1f7c3fa429e7e057ab12948167f4a394e3ae7d95
Hoyann <hoyann@vip.qq.com>
2016-03-08 09:43:04 +0800

    html theme

--- docs/cbuild/html/_sources/index.txt
+++ docs/cbuild/html/_sources/index.txt
@@ -1,5 +1,5 @@
 .. ChangMao documentation master file, created by
-   sphinx-quickstart on Mon Mar 07 18:19:45 2016.
+   sphinx-quickstart on Mon Mar 07 18:27:36 2016.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # ChangMao documentation build configuration file, created by
-# sphinx-quickstart on Mon Mar 07 18:19:45 2016.
+# sphinx-quickstart on Mon Mar 07 18:27:36 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -29,7 +29,16 @@ import shlex
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ctemplates']
@@ -101,14 +110,14 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+todo_include_todos = True
 
 
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -282,3 +291,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/': None}
